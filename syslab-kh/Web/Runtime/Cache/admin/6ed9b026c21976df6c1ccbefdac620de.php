2e3b5d830e8b61d4ee482830909a5e10b815f918
yanhan19940405 <1257157255@qq.com>
2015-04-11 11:13:31 +0800

    前端页面

--- syslab-kh/Web/Runtime/Cache/admin/6ed9b026c21976df6c1ccbefdac620de.php
+++
@@ -1,6 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?><html>
-<body>
-	<h1>Welcome <?php echo $_POST['name'] ?></h1>
-</body>
-
-</html>
--- syslab-kh/Web/Runtime/Cache/admin/990ac7ccd8fd2be2ea4788a5f04d1734.php
+++
@@ -1,1 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit(); echo '这是你刚才注册的信息'; echo $_POST['name']; ?>
